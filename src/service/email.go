1c6b1919b621c0d26ee0b735119ebce2a5dc1b40
Unedo Tampubolon <unedo.tampubolon@gmail.com>
2024-02-02 23:03:24 +0700

    feat: add email service

---
+++ src/service/email.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"github.com/unedtamps/go-backend/config"
+	"github.com/unedtamps/go-backend/util"
+	"gopkg.in/gomail.v2"
+)
+
+var dialer *gomail.Dialer
+
+type emailService struct {
+	from    string
+	to      string
+	subject string
+	body    string
+}
+
+func newEmail(subject string, to string, body <-chan string) *emailService {
+	return &emailService{
+		from:    config.Env.EmailSender,
+		to:      to,
+		subject: subject,
+		body:    <-body,
+	}
+}
+
+func (e *emailService) Send() {
+	m := gomail.NewMessage()
+	m.SetHeader("From", e.from)
+	m.SetHeader("To", e.to)
+	m.SetHeader("Subject", e.subject)
+	m.SetBody("text/html", e.body)
+	err := dialer.DialAndSend(m)
+	if err != nil {
+		util.Log.Info("Error sending email: ", err)
+	} else {
+		util.Log.Info("Email sent successfully to ", e.to)
+	}
+}
---
+++ util/parser.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"bytes"
+	"text/template"
+)
+
+type EmailConfirm struct {
+	Id    string
+	Name  string
+	Email string
+}
+
+func ParseAccountComfirmation(em EmailConfirm) string {
+	var a bytes.Buffer
+	tpl := template.Must(template.ParseFiles("template/email_confirm.html"))
+
+	err := tpl.Execute(&a, em)
+	if err != nil {
+		Log.Fatal("Error parsing email confirmation template: ", err)
+	}
+	return a.String()
+}

c47d917e60846b7fe080960f66e16346602a0516
Unedo Tampubolon <unedo.tampubolon@gmail.com>
2024-03-04 21:30:45 +0700

    fix: go-test and error response

--- util/error.go
+++ util/error.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -15,25 +14,25 @@ func ErrorHandler(err error) interface{} {
 }
 
 func UnknownError(c *gin.Context, err error) {
-	c.JSON(http.StatusInternalServerError, ErrorHandler(fmt.Errorf("Unknown Error: %v", err)))
+	c.JSON(http.StatusInternalServerError, ErrorHandler(err))
 }
 
 func LimitError(c *gin.Context, err error) {
-	c.JSON(http.StatusTooManyRequests, ErrorHandler(fmt.Errorf("Limit Error: %v", err)))
+	c.JSON(http.StatusTooManyRequests, ErrorHandler(err))
 }
 
 func BadRequest(c *gin.Context, err error) {
-	c.JSON(http.StatusBadRequest, ErrorHandler(fmt.Errorf("Bad Request: %v", err)))
+	c.JSON(http.StatusBadRequest, ErrorHandler(err))
 }
 
 func UnauthorizedError(c *gin.Context, err error) {
-	c.JSON(http.StatusUnauthorized, ErrorHandler(fmt.Errorf("Unauthorized: %v", err)))
+	c.JSON(http.StatusUnauthorized, ErrorHandler(err))
 }
 
 func NotFoundError(c *gin.Context, err error) {
-	c.JSON(http.StatusNotFound, ErrorHandler(fmt.Errorf("Not Found: %v", err)))
+	c.JSON(http.StatusNotFound, ErrorHandler(err))
 }
 
 func ForbiddenError(c *gin.Context, err error) {
-	c.JSON(http.StatusForbidden, ErrorHandler(fmt.Errorf("Forbidden :%v", err)))
+	c.JSON(http.StatusForbidden, ErrorHandler(err))
 }
